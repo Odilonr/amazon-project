6a641acaaabba1c9d4043a393f3fc527c5395cbf
Odilonr <rwabukambao@gmail.com>
2024-09-10 20:56:49 -0500

    cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -70,6 +70,13 @@ document.querySelectorAll('.js-add-to-cart')
         })
       }
       
-      console.log(cart)
+      let totalQuantity = 0
+
+      cart.forEach( (product) => {
+        totalQuantity += product.quantity
+      })
+
+      document.querySelector('.js-cart-quantity').innerHTML = totalQuantity
+      
     })
   })
