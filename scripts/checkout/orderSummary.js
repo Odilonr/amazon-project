64f50227ec2a814d2b756aa45e3a7fcdd5ebcd04
Odilonr <rwabukambao@gmail.com>
2024-09-13 09:46:27 -0500

    tested more

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -24,7 +24,7 @@ export function renderOrderSummary () {
     const dateString = calculateDeliveryDate(deliveryOption)
 
     cartSummaryHTML +=`
-      <div class="cart-item-container js-cart-item-container-${productId}">
+      <div class="cart-item-container js-cart-item-container js-cart-item-container-${productId}">
             <div class="delivery-date">
               Delivery date: ${dateString}
             </div>
@@ -40,7 +40,7 @@ export function renderOrderSummary () {
                 <div class="product-price">
                   $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                   <span>
                     Quantity: <span class="quantity-label js-quantity-label-${productId}">
                     ${cartItem.quantity}</span>
@@ -52,7 +52,7 @@ export function renderOrderSummary () {
                   <input class="quantity-input js-quantity-input-${matchingProduct.id}">
                   <span class="save-quantity-link link-primary js-save-link"
                   data-product-id=${matchingProduct.id}>Save</span>
-                  <span class="delete-quantity-link link-primary js-delete-link"
+                  <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}"
                   data-product-id=${matchingProduct.id}>
                     Delete
                   </span>
