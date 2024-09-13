2c48a13b3742f877278502542e85d9732aa9e739
Odilonr <rwabukambao@gmail.com>
2024-09-12 20:08:01 -0500

    add jasmine testing framework

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,7 +1,44 @@
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js"
+
 export function getDeliveryOption (deliveryOptionId) {
   const deliveryOption = deliveryOptions.find(
         option => option.id == deliveryOptionId)
   return deliveryOption || deliveryOptions[0]
+}
+
+export function calculateDeliveryDate (deliveryOption) {
+  let deliveryDate = dayjs().add(2,'days')
+  let timer = deliveryOption.deliveryDays
+  
+
+  while(timer > 0) {
+    let dayofWeek = deliveryDate.format('dddd')
+
+    if (!isWeekend(dayofWeek)) {
+      timer --
+    }
+
+    if (timer > 0) {
+      deliveryDate = deliveryDate.add(1,'days')
+    }
+  } 
+
+  const finalDayWeek = deliveryDate.day()
+
+  if(finalDayWeek === 6) {
+    deliveryDate = deliveryDate.add(2, 'days')
+  } else if (finalDayWeek === 0) {
+    deliveryDate = deliveryDate.add(1, 'days')
+  }
+
+
+  return deliveryDate.format('dddd, MMMM D')
+
+ 
+}
+
+export function isWeekend (day) {
+  return day === 'Saturday' || day === 'Sunday'
 }
 
 
@@ -11,7 +48,7 @@ export const deliveryOptions = [{
   priceCents: 0
 },{
   id:'2',
-  deliveryDays: 0,
+  deliveryDays: 3,
   priceCents: 499
 },{
   id:'3', 
